9f80ad493f6f706638b7418f6293741eb2ec2083
Evgeniy Belyaev <eb.gh.cr@gmail.com>
2018-07-13 16:33:14 +0400

    init

---
+++ lib/yandex_translate.rb
@@ -0,0 +1,5 @@
+require "yandex_translate/version"
+
+module YandexTranslate
+  # Your code goes here...
+end
---
+++ spec/yandex_translate_spec.rb
@@ -0,0 +1,9 @@
+RSpec.describe YandexTranslate do
+  it "has a version number" do
+    expect(YandexTranslate::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
